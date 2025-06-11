b31b1c0b8d3955fc0501ae03be3682e66a2f3800
Lopezzz56 <frank.lopes@xaviers@edu.in>
2025-06-11 12:49:49 +0530

    Fix GitHub Pages deployment path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ecomm-reactjs/', 
   plugins: [react(),     
     tailwindcss(),
 ],
